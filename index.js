7153de0ea7b6e35f1919f3f31b7e01f13931d504
Gevionielly <68006269+Gevionielly@users.noreply.github.com>
2023-07-03 11:39:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.once(Events.ClientReady, c => {
       .catch(error => console.log(error));
 
     console.log('[REQUEST] Request made');
-  }, 1000); // Interval in milliseconds
+  }, 10000); // Interval of request and status update of the bot in milliseconds
 });
 
 client.login(token);
