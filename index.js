5e660bcfd136e15538e1cb9e2ac395b0142cd338
ooarai <68006269+Gevionielly@users.noreply.github.com>
2023-07-23 12:48:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-const { ip } = require('./config.json');
-const { platform } = require('./config.json');
-const { ActivityType } = require('discord.js');
+const { ActivityType, Client, Events, GatewayIntentBits } = require('discord.js');
+const { token, ip, platform, mode } = require('./config.json');
 const fetch = require('node-fetch');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -14,8 +11,22 @@ client.once(Events.ClientReady, c => {
     fetch(`https://api.mcstatus.io/v2/status/${platform}/${ip}`)
       .then(response => response.json())
       .then(data => {
-        const onlineStatus = data.online ? `Online [${data.players.online}/${data.players.max}]` : 'Offline';
-        client.user.setActivity(`${onlineStatus}`, { type: ActivityType.Playing });
+        if (mode === "aternos") {
+          const motdLowerCase = data.motd.clean.toLowerCase();
+          const isOffline = motdLowerCase.includes('this server is offline.');
+
+          if (isOffline) {
+            client.user.setActivity('Offline', { type: ActivityType.Playing });
+          } else {
+            const onlineStatus = data.online ? `Online [${data.players.online}/${data.players.max}]` : 'Offline';
+            client.user.setActivity(`${onlineStatus}`, { type: ActivityType.Playing });
+          }
+        } else if (mode === "normal") {
+          const onlineStatus = data.online ? `Online [${data.players.online}/${data.players.max}]` : 'Offline';
+          client.user.setActivity(`${onlineStatus}`, { type: ActivityType.Playing });
+        } else {
+          console.log(`Invalid mode value in config.json: ${mode}`);
+        }
       })
       .catch(error => console.log(error));
 
